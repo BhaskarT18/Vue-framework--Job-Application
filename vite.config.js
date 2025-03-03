b5551ccf42e8c23ffbb1878d90034a24e6f80a2f
BhaskarT18 <bhaskartikale@gmail.com>
2025-03-03 11:37:51 +0530

    added  config file

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   server: {
+    host: "0.0.0.0", 
     port: 3000,
     proxy: {
       "/api": {
